a68fbae4d2c949589537e086e7426854c22e255c
AnnBalias <annbalias@gmail.com>
2025-07-02 18:13:36 +0300

    appointment-modal-style

--- src/components/AppointmentModal/AppointmentModal.jsx
+++ src/components/AppointmentModal/AppointmentModal.jsx
@@ -1,5 +1,22 @@
+import CloseSvg from '../../assets/icons/close.svg';
+import css from './AppointmentModal.module.css';
+
 const AppointmentModal = () => {
-  return <p>AppointmentModal</p>;
+  return (
+    <div className={css.base}>
+      <button className={css.closeBtn}>
+        <img src={CloseSvg} alt="Close button" className={css.closeSvg} />
+      </button>
+      <h2 className={css.title}>Make an appointment with a psychologists</h2>
+      <p className={css.text}>
+        You are on the verge of changing your life for the better. Fill out the
+        short form below to book your personal appointment with a professional
+        psychologist. We guarantee confidentiality and respect for your privacy.
+      </p>
+      <p>PSYCHOLOGIST</p>
+      <p>APPOINTMENT FORM</p>
+    </div>
+  );
 };
 
 export default AppointmentModal;
