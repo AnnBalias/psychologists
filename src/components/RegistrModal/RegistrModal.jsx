3cd079e6add9b1d7637e3e0c6b47fb06361096a6
AnnBalias <annbalias@gmail.com>
2025-07-02 18:02:10 +0300

    register-modal-style

--- src/components/RegistrModal/RegistrModal.jsx
+++ src/components/RegistrModal/RegistrModal.jsx
@@ -1,5 +1,20 @@
+import CloseSvg from '../../assets/icons/close.svg';
+import css from './RegistrModal.module.css';
+
 const RegistrModal = () => {
-  return <p>RegistrModal</p>;
+  return (
+    <div className={css.base}>
+      <button className={css.closeBtn}>
+        <img src={CloseSvg} alt="Close button" className={css.closeSvg} />
+      </button>
+      <h2 className={css.title}>Registration</h2>
+      <p className={css.text}>
+        Thank you for your interest in our platform! In order to register, we
+        need some information. Please provide us with the following information.
+      </p>
+      <p>REGISTR FORM</p>
+    </div>
+  );
 };
 
 export default RegistrModal;
