32e5b3494fe6e0d095a036618a3ec52e83df4044
AnnBalias <annbalias@gmail.com>
2025-07-24 13:49:28 +0300

    no-scroll-when-modal-open

--- src/components/ModalWrapper/ModalWrapper.jsx
+++ src/components/ModalWrapper/ModalWrapper.jsx
@@ -3,14 +3,28 @@ import css from './ModalWrapper.module.css';
 
 const ModalWrapper = ({ component, onClose }) => {
   useEffect(() => {
+    document.body.style.position = 'fixed';
+    document.body.style.top = '0';
+    document.body.style.left = '0';
+    document.body.style.right = '0';
+    document.body.style.overflowY = 'scroll';
+
     const handleKeyDown = (e) => {
       if (e.key === 'Escape') {
         onClose();
       }
     };
-
     document.addEventListener('keydown', handleKeyDown);
-    return () => document.removeEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.left = '';
+      document.body.style.right = '';
+      document.body.style.overflowY = '';
+      window.scrollTo(0, scrollY);
+      document.removeEventListener('keydown', handleKeyDown);
+    };
   }, [onClose]);
 
   return (
