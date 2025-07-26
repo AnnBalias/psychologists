7eb9c1f9382a912d18e09fe6b6e4889f17a88944
AnnBalias <annbalias@gmail.com>
2025-07-26 16:18:44 +0300

    lazy-import

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import HomePage from './pages/HomePage/HomePage';
-import PsychologistsPage from './pages/PsychologistsPage/PsychologistsPage';
-import { Toaster } from 'react-hot-toast';
-import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
 import PrivateRoute from './PrivateRoute';
+import { Toaster } from 'react-hot-toast';
+import './App.css';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const PsychologistsPage = lazy(() =>
+  import('./pages/PsychologistsPage/PsychologistsPage')
+);
+const FavoritesPage = lazy(() => import('./pages/FavoritesPage/FavoritesPage'));
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/psychologists" element={<PsychologistsPage />} />
-        <Route
-          path="/favorites"
-          element={
-            <PrivateRoute>
-              <FavoritesPage />
-            </PrivateRoute>
-          }
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/psychologists" element={<PsychologistsPage />} />
+          <Route
+            path="/favorites"
+            element={
+              <PrivateRoute>
+                <FavoritesPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          toastOptions={{
+            duration: 4000,
+          }}
         />
-      </Routes>
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        toastOptions={{
-          duration: 4000,
-        }}
-      />
+      </Suspense>
     </>
   );
 }
